4e37013ce05f3f08844a528880dbc30da0a75cba
Brandon Alejandro Mosqueda González <brandonMG_1698@hotmail.com>
2018-03-18 13:12:12 -0600

    cambios visual studio code

--- includes/views/header.inc.php
+++ includes/views/header.inc.php
@@ -8,19 +8,33 @@
   	<script src="<?php echo JQUERY_PATH.DS.'jquery.js' ?>"></script>
   	<link href="<?php echo BOOTSTRAP_PATH.DS.'css'.DS.'bootstrap.min.css' ?>" rel="stylesheet" media="screen"></style>
   	<script src="<?php echo BOOTSTRAP_PATH.DS.'js'.DS.'bootstrap.min.js'?>"></script>
-  	<script type="text/javascript">
-  		
-  	</script>
+  	<script type="text/javascript"></script>
+  	<style type="text/css">
+  		.cabecera
+  		{
+  			width: 100%; /* hacemos que la cabecera ocupe el ancho completo de la página */
+			left: 0; /* Posicionamos la cabecera al lado izquierdo */
+			top: 136; /* Posicionamos la cabecera pegada arriba */
+			position: fixed; /* Hacemos que la cabecera tenga una posición fija */
+			height: 140px;
+			padding-left: 15px
+  		}
+  	</style>
 <head>
 	
 <body>
 
-	<div class="container-fluid" style="background-color: #70A225">
+	<div class="cabecera" style="background-color: #70A225">
 		<img src="<?php echo IMAGES_PATH.'logoUdC.png'?>" alt="Logo Universidad de Colima" 
 			href="<?php echo VIEW_PATH.'index.wiew.php'?>" style=" width: 616px; height: 135px;">
+		<div class="cabecera" style="background-color: white; height: 80px; width: 100%">
+			<button type="button" class="btn btn-primary newPost" data-toggle="modal" 
+					data-target="#addBlog">Nuevo</button>
+		</div>
 	</div>
 		<br>
-	<div class="container-fluid">
-		<button type="button" class="btn btn-primary newPost" data-toggle="modal" 
-				data-target="#addBlog">Nuevo</button>
+	<div class="container-fluid" style="z-index: 1100;">
+		<br><br>
+		<br><br>
+		<br><br>
 		<br><br>
